07615a4bd195e75b603ff75d77f1c7c10a102e1e
wenlin <4489990@qq.com>
2015-01-18 19:48:04 +0800

    Initial Commit

---
+++ queen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  queen
+//
+//  Created by wenlin on 15/1/18.
+//  Copyright (c) 2015年 BRYQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
